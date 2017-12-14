966ba17a23e993dab12df94574643ddaf7d7b611
Karine Bialobroda <karine.bialobroda@gmail.com>
2017-12-14 12:23:03 +0100

    test config

--- config.rb
+++ config.rb
@@ -1,56 +1,17 @@
 require 'json'
 require 'open-uri'
 
-###
-# Compass
-###
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+activate :sprockets
 
-# Auto-prefixing of CSS code with vendor prefix
-activate :autoprefixer
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 data.portfolio.keys.each do |project|
   proxy "/work/#{project}.html", "/work/show.html", :locals => { project: project }, :ignore => true
 end
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
 helpers do
   def color
     ["blue", "orange", "red"].sample
@@ -76,32 +37,17 @@ helpers do
   end
 end
 
-set :css_dir, 'stylesheets'
 
-set :js_dir, 'javascripts'
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-set :images_dir, 'images'
-
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
   activate :relative_assets
   set :relative_links, true
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-  #
-  require_relative "./lib/build_cleaner"
-  activate :build_cleaner
 end
 
 # Sitemap
@@ -111,12 +57,12 @@ activate :search_engine_sitemap
 
 # Deployment
 activate :deploy do |deploy|
-  deploy.method = :git
   deploy.build_before = true
-
-  # Optional Settings
-  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.deploy_method = :git
 end
+
+
+
+
+
+
